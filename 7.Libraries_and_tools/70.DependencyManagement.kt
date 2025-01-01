22b56e25209a260e742992b21eea6397554ffefe
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-02 01:01:06 +0700

    Add 70.DependencyManagement.kt file

---
+++ 7.Libraries_and_tools/70.DependencyManagement.kt
@@ -0,0 +1,30 @@
+// Đây là ví dụ về việc quản lý dependency trong file build.gradle:
+dependencies {
+    implementation 'org.jetbrains.kotlin:kotlin-stdlib:1.5.31'
+    implementation 'io.ktor:ktor-client-cio:2.0.0'
+    implementation 'com.squareup.moshi:moshi:1.12.0'
+}
+// Đây là ví dụ về việc quản lý dependency trong file build.gradle.kts:
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.5.31")
+    implementation("io.ktor:ktor-client-cio:2.0.0")
+    implementation("com.squareup.moshi:moshi:1.12.0")
+}
+// Đây là ví dụ về việc quản lý dependency trong file build.gradle.kts sử dụng Kotlin DSL:
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.5.31")
+    implementation("io.ktor:ktor-client-cio:2.0.0")
+    implementation("com.squareup.moshi:moshi:1.12.0")
+}
+// Đây là ví dụ về việc quản lý dependency trong file build.gradle.kts sử dụng Kotlin DSL:
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.5.31")
+    implementation("io.ktor:ktor-client-cio:2.0.0")
+    implementation("com.squareup.moshi:moshi:1.12.0")
+}
+// Đây là ví dụ về việc quản lý dependency trong file build.gradle.kts sử dụng Kotlin DSL:
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.5.31")
+    implementation("io.ktor:ktor-client-cio:2.0.0")
+    implementation("com.squareup.moshi:moshi:1.12.0")
+}
