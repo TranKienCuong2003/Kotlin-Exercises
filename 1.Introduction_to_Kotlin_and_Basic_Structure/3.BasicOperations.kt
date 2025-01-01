2c4ae325d3923b5f9fa1dc0fa3a2d15acc3d8b60
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-02 00:17:40 +0700

    Add 3.BasicOperations.kt file

---
+++ 1.Introduction_to_Kotlin_and_Basic_Structure/3.BasicOperations.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val a = 10
+    val b = 5
+
+    println("Phep cong: ${a + b}")
+    println("Phep tru: ${a - b}")
+    println("Phep nhan: ${a * b}")
+    println("Phep chia: ${a / b}")
+    println("Phep chia du: ${a % b}")
+}
