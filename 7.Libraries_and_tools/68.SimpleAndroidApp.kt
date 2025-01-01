726644cf293a7fd6a390429d6f2a80e768e4c599
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-02 00:59:39 +0700

    Add 68.SimpleAndroidApp.kt file

---
+++ 7.Libraries_and_tools/68.SimpleAndroidApp.kt
@@ -0,0 +1,13 @@
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textView: TextView = findViewById(R.id.textView)
+        textView.text = "Hello, Android!"
+    }
+}
