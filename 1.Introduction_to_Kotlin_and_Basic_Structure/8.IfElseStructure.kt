dc735dee2654c237ce222999acd45ddab5dbc8f7
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-02 00:20:28 +0700

    Add 8.IfElseStructure.kt flie

---
+++ 1.Introduction_to_Kotlin_and_Basic_Structure/8.IfElseStructure.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val number = 10
+
+    if (number > 0) {
+        println("So duong")
+    } else if (number < 0) {
+        println("So am")
+    } else {
+        println("So khong")
+    }
+}
