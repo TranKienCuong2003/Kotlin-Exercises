2f5f0b31d617137bc496752c3bb835a16eb5e40b
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-02 00:18:41 +0700

    Add 4.PrintValues.kt file

---
+++ 1.Introduction_to_Kotlin_and_Basic_Structure/4.PrintValues.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val name: String = "Kotlin"
+    val age: Int = 2025
+    val pi: Double = 3.14159
+
+    println("Chuoi: $name")
+    println("So nguyen: $age")
+    println("So thuc: $pi")
+}
