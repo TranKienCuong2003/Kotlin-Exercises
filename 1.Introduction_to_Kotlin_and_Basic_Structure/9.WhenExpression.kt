6536043edc50de7957abce87468c8457f04d529c
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-02 00:20:52 +0700

    Add 9.WhenExpression.kt file

---
+++ 1.Introduction_to_Kotlin_and_Basic_Structure/9.WhenExpression.kt
@@ -0,0 +1,14 @@
+fun main() {
+    val day = 3
+
+    when (day) {
+        1 -> println("Thu Hai")
+        2 -> println("Thu Ba")
+        3 -> println("Thu Tu")
+        4 -> println("Thu Nam")
+        5 -> println("Thu Sau")
+        6 -> println("Thu Bay")
+        7 -> println("Chu Nhat")
+        else -> println("Khong hop le")
+    }
+}
