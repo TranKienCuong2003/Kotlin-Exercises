45beac019ceaefb0f334f151ddf153cecaf9867c
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-02 00:19:37 +0700

    Add 6.ValVsVar.kt flie

---
+++ 1.Introduction_to_Kotlin_and_Basic_Structure/6.ValVsVar.kt
@@ -0,0 +1,9 @@
+fun main() {
+    var mutableVariable = 100
+    println("Bien thay dooi: $mutableVariable")
+    mutableVariable = 200
+    println("Bien thay dooi sau khi gan lai: $mutableVariable")
+
+    val immutableVariable = "Kotlin"
+    println("Bien khong doi: $immutableVariable")
+}
