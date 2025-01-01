ee663a1dbc768fa8faa16d981009c5378ff1f911
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-02 00:20:02 +0700

    Add 7.TypeConversion.kt file

---
+++ 1.Introduction_to_Kotlin_and_Basic_Structure/7.TypeConversion.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val intVal: Int = 100
+    val doubleVal: Double = intVal.toDouble()
+    val stringVal: String = intVal.toString()
+
+    println("Chuyen Int sang Double: $doubleVal")
+    println("Chuyen Int sang String: $stringVal")
+}
